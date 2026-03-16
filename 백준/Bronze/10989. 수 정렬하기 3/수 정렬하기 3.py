c230a0715458774568e1a16213c90087d90aa720
박승일 (Park Seungil) <s24060@gsm.hs.kr>
2026-03-16 09:25:27 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 7876 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+count = [0] * 10001
+
+for _ in range(n):
+    count[int(input())] += 1
+
+for i in range(1, 10001):
+    if count[i] != 0:
+        for _ in range(count[i]):
+            print(i)
